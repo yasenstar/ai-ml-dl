2a58378f272fa379264d136ce6ac86430cdb2092
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-27 13:18:52 +0800

    add mindmaps

---
+++ DL/Python快乐编程-人工智能深度学习基础.mm
@@ -0,0 +1,168 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Python快乐编程&#xa;人工智能深度学习基础" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1732678656008"><hook NAME="MapStyle" background="#f5f5dcff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_sepia_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-10.5 pt;" ENDINCLINATION="81.75 pt;19.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_411331447" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_411331447"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#2c2b29" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="1. 深度学习简介" FOLDED="true" POSITION="bottom_or_right" ID="ID_1577933840" CREATED="1732678670534" MODIFIED="1732678678474">
+<node TEXT="1.1 什么是机器学习" ID="ID_780520003" CREATED="1732678825955" MODIFIED="1732678831515"/>
+<node TEXT="1.2 什么是深度学习" FOLDED="true" ID="ID_295303771" CREATED="1732678831826" MODIFIED="1732678837796">
+<node TEXT="深度学习让计算机可以通过组合低层次特征形成更加抽象的高层次特征（或属性类别）​" ID="ID_745523007" CREATED="1732678872889" MODIFIED="1732678877132"/>
+<node TEXT="1.2.1 深度学习的发展" ID="ID_282335702" CREATED="1732678947961" MODIFIED="1732678954010">
+<node TEXT="1）推理期" ID="ID_1204861557" CREATED="1732678954010" MODIFIED="1732678964439"/>
+<node TEXT="2）知识期" ID="ID_1150293921" CREATED="1732678965105" MODIFIED="1732678982125"/>
+<node TEXT="3）学习期" ID="ID_1425638735" CREATED="1732679007203" MODIFIED="1732679014753"/>
+<node TEXT="4）快速发展期" ID="ID_1447405633" CREATED="1732679066243" MODIFIED="1732679072272"/>
+<node TEXT="5）爆发期" ID="ID_500272431" CREATED="1732679111781" MODIFIED="1732679116413"/>
+</node>
+<node TEXT="1.2.2 深度学习的3个层次" ID="ID_1814030223" CREATED="1732679204323" MODIFIED="1732679212348">
+<node TEXT="在《论语·阳货》中提到“性相近也，习相远也”​，这句话同样适用于机器学习领域。机器学习的对象是数据，数据是否带有标签，会对机器学习最后习得的“习性”产生影响，​“习染积久”的环境不一样，其表现出来的“习性”也有所不同" ID="ID_418485821" CREATED="1732679237208" MODIFIED="1732679242552"/>
+<node TEXT="1）监督学习 Supervised Learning" ID="ID_1104679401" CREATED="1732679269880" MODIFIED="1732679280858">
+<node TEXT="美国伊利诺伊大学香槟分校计算机系的韩家炜（Jiawei Han）教授认为监督学习可以被看作“分类”​（classification）的代名词。计算机从有标签的训练数据中学习，然后给定某个新数据，预测这个新数据的标签，标签（label）是指某个事物所属的类别" ID="ID_1307210061" CREATED="1732679310663" MODIFIED="1732679317041"/>
+<node TEXT="监督学习的工作，就是通过有标签的数据训练，构建一个模型，然后通过构建的模型，给新数据添加上特定的标签。" ID="ID_1489275534" CREATED="1732679350228" MODIFIED="1732679353838"/>
+<node TEXT="机器学习的目标可以概括为：让计算机通过学习不断完善构建的模型，让构建的模型更好地适用于“新样本”​，而不是仅仅在训练样本上工作得更好。通过训练构建的模型适用于新样本的能力，称为泛化（generalization）能力" ID="ID_926228321" CREATED="1732679367627" MODIFIED="1732679370296"/>
+</node>
+<node TEXT="2）无监督学习 Unsupervised Learning" ID="ID_33656465" CREATED="1732679372298" MODIFIED="1732679388565">
+<node TEXT="无监督学习中模型所学习的数据都是无标签的，根据类别未知的训练样本解决模式识别中的各种问题。无监督学习可以被看作聚类（cluster）的近义词" ID="ID_1875543052" CREATED="1732679415524" MODIFIED="1732679420431"/>
+</node>
+<node TEXT="3）半监督学习 Semi-Supervised Learning" ID="ID_1454354561" CREATED="1732679438018" MODIFIED="1732679454983">
+<node TEXT="半监督环境是先在有监督的环境下初步构建好模型后再进行无监督学习。" ID="ID_342495482" CREATED="1732679473430" MODIFIED="1732679476050"/>
+<node TEXT="半监督学习就是先用带有标签的数据帮助计算机初步构建模型，然后让计算机根据已有的模型去学习无标签的数据。需要注意的是，这里隐含了一个基本假设——“聚类假设”​（Cluster Assumption）​，即相似的样本拥有相似的输出。" ID="ID_887826088" CREATED="1732679517728" MODIFIED="1732679521044"/>
+</node>
+</node>
+<node TEXT="1.2.3 深度学习的3种结构类型" ID="ID_1874821712" CREATED="1732679531856" MODIFIED="1732679543734">
+<node TEXT="1）生成型深度结构" ID="ID_1263692711" CREATED="1732683543835" MODIFIED="1732683557500">
+<node TEXT="自编码器" ID="ID_175754999" CREATED="1732683750258" MODIFIED="1732683760548">
+<arrowlink DESTINATION="ID_606774198"/>
+</node>
+<node TEXT="受限玻尔兹曼机" ID="ID_1038811672" CREATED="1732683762941" MODIFIED="1732683772028">
+<arrowlink DESTINATION="ID_1639709038"/>
+</node>
+</node>
+<node TEXT="2）判别型深度结构" ID="ID_348037565" CREATED="1732683558560" MODIFIED="1732683567689">
+<node TEXT="卷积神经网络" ID="ID_721768974" CREATED="1732683788910" MODIFIED="1732683797956">
+<arrowlink DESTINATION="ID_1884217427"/>
+</node>
+</node>
+<node TEXT="3）混合型深度结构" ID="ID_1714179996" CREATED="1732683695413" MODIFIED="1732683703634"/>
+</node>
+</node>
+<node TEXT="1.3 深度学习的研究现状" ID="ID_500791203" CREATED="1732678838669" MODIFIED="1732678847019">
+<node TEXT="语音识别领域" ID="ID_1721520865" CREATED="1732683856788" MODIFIED="1732683861670">
+<node TEXT="高斯混合模型 Gauss Mixture Model, GMM" ID="ID_1281008958" CREATED="1732683861675" MODIFIED="1732683873243"/>
+</node>
+<node TEXT="图像识别领域" ID="ID_1109206443" CREATED="1732683911531" MODIFIED="1732683919377">
+<node TEXT="卷积神经网络" ID="ID_1508112442" CREATED="1732683919381" MODIFIED="1732683931440"/>
+</node>
+<node TEXT="自然语言处理领域" ID="ID_1831241148" CREATED="1732683961489" MODIFIED="1732683974185">
+<node TEXT="人工神经网络" ID="ID_1722483389" CREATED="1732683974185" MODIFIED="1732683980373"/>
+</node>
+</node>
+</node>
+<node TEXT="2. Theano基础" POSITION="bottom_or_right" ID="ID_567455661" CREATED="1732678678630" MODIFIED="1732678683655">
+<node TEXT="2.1 初识Theano" ID="ID_309714586" CREATED="1732684043355" MODIFIED="1732684060168"/>
+<node TEXT="2.2 安装Theano" ID="ID_656896885" CREATED="1732684060529" MODIFIED="1732684065603"/>
+<node TEXT="2.3 配置环境变量" ID="ID_1470159290" CREATED="1732684065968" MODIFIED="1732684071201"/>
+<node TEXT="2.4 Theano中的符号变量" ID="ID_643185374" CREATED="1732684071394" MODIFIED="1732684084000"/>
+<node TEXT="2.5 Theano编程风格" ID="ID_1253866972" CREATED="1732684085128" MODIFIED="1732684092001"/>
+<node TEXT="2.6 Theano中的函数" ID="ID_311527496" CREATED="1732684092374" MODIFIED="1732684103352">
+<node TEXT="2.6.1 函数的定义" ID="ID_1198887121" CREATED="1732684104922" MODIFIED="1732684110499"/>
+<node TEXT="2.6.2 函数的复制" ID="ID_588622535" CREATED="1732684110666" MODIFIED="1732684116282"/>
+<node TEXT="2.6.3 Theano中重要的函数" ID="ID_1812123392" CREATED="1732684117055" MODIFIED="1732684132295"/>
+</node>
+<node TEXT="2.7 Theano中的符合计算图模型" ID="ID_1537097988" CREATED="1732684133056" MODIFIED="1732684147831"/>
+<node TEXT="2.8 Theano中的条件表达式" ID="ID_520000899" CREATED="1732684149511" MODIFIED="1732684158146"/>
+<node TEXT="2.9 Theano中的循环" ID="ID_207304518" CREATED="1732684158328" MODIFIED="1732684167713"/>
+<node TEXT="2.10 Theano中的常用Debug技巧" ID="ID_1301866989" CREATED="1732684170865" MODIFIED="1732684181775"/>
+</node>
+<node TEXT="3. 线性代数基础" POSITION="bottom_or_right" ID="ID_206178383" CREATED="1732678683869" MODIFIED="1732678694262"/>
+<node TEXT="4. 概率与信息论" POSITION="bottom_or_right" ID="ID_1299292505" CREATED="1732678694428" MODIFIED="1732678705802"/>
+<node TEXT="5. 深度学习基础知识" POSITION="bottom_or_right" ID="ID_1152728093" CREATED="1732678709789" MODIFIED="1732678715433"/>
+<node TEXT="6. 数值计算与最优化" POSITION="bottom_or_right" ID="ID_1690754728" CREATED="1732678718948" MODIFIED="1732678728528"/>
+<node TEXT="7. 概率图模型" POSITION="bottom_or_right" ID="ID_539628090" CREATED="1732678730973" MODIFIED="1732678736405"/>
+<node TEXT="8. 前馈神经网络" POSITION="bottom_or_right" ID="ID_1071676640" CREATED="1732678740691" MODIFIED="1732678747302"/>
+<node TEXT="9. 反向传播与梯度计算" POSITION="bottom_or_right" ID="ID_1936999703" CREATED="1732678747636" MODIFIED="1732678761098"/>
+<node TEXT="10. 自编码器" POSITION="bottom_or_right" ID="ID_606774198" CREATED="1732678750020" MODIFIED="1732678779535"/>
+<node TEXT="11. 玻尔兹曼机及其相关模型" POSITION="bottom_or_right" ID="ID_1639709038" CREATED="1732678780283" MODIFIED="1732678792814"/>
+<node TEXT="12. 循环神经网络" POSITION="bottom_or_right" ID="ID_30843301" CREATED="1732678795576" MODIFIED="1732678803860"/>
+<node TEXT="13. 递归神经网络" POSITION="bottom_or_right" ID="ID_1301710546" CREATED="1732678806234" MODIFIED="1732678811590"/>
+<node TEXT="14. 卷积神经网络CNN&#xa;Convolutional Neural Network" POSITION="bottom_or_right" ID="ID_1884217427" CREATED="1732678813961" MODIFIED="1732679167014"/>
+</node>
+</map>
---
+++ DL/README.md
@@ -0,0 +1,7 @@
+# Deep Learning 深度学习
+
+## Python 快乐编程 人工智能—深度学习基础
+
+![book_cover](img/python-happy-coding_DL.png)
+
+Purchase at here: http://www.tup.tsinghua.edu.cn/wap/tsxqy.aspx?id=07866201
