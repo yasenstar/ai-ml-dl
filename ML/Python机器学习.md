56e19014b435c4e281d578c417f5b6fead399d91
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-27 20:11:51 +0800

    add Python机器学习 思维导图

---
+++ ML/Python机器学习.md
@@ -0,0 +1,60 @@
+ Python机器学习
+(III, 2021)
+
+# 01. 赋予计算机从数据中学习的能力
+
+# 02. 训练简单的机器学习分类算法
+
+# 03. scikit-learn机器学习分类器
+
+# 04. 构建良好的训练数据集--数据预处理
+
+# 05. 通过降维压缩数据
+
+# 06. 模型评估和超参数调优的最佳实践
+
+# 07. 组合不同模型的集成学习
+
+# 08. 用机器学习进行情感分析
+
+# 09. 将机器学习模型嵌入Web应用
+
+# 10. 用回归分析预测连续目标变量
+
+# 11. 用聚类分析处理无标签数据
+
+# 12. 从零开始实现多层人工神经网络
+
+# 13. 同TensorFlow并行训练神经网络
+
+# 14. 深入探讨TensorFlow的工作原理
+
+# 15. 用深度卷积神经网络为图像分类
+
+# 16. 用循环神经网络为序列数据建模
+
+# 17. 用生成对抗网络GAN合成新数据
+
+# 18. 用于复杂环境决策的强化学习
+
+# Experts
+
+## Geoffrey E. Hinton
+
+## Andrew Ng
+
+## Yann Lecun
+
+## Juergen Schmidhuber
+
+## Yoshua Bengio
+
+## Sebastian Raschka (author)
+
+# Python Library
+
+## scikit-learn
+
+## Keras
+
+## TensorFlow
---
+++ ML/Python机器学习.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Python机器学习&#xa;(III, 2021)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1732708740960" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8ff" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_sky_element_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_4172522" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.7 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_4172522" STARTINCLINATION="116.25 pt;0 pt;" ENDINCLINATION="116.25 pt;28.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#051552" WIDTH="2" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fff024" BACKGROUND_COLOR="#000000"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc7212" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1823054225" COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#9d5e19">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#9d5e19" TRANSPARENCY="255" DESTINATION="ID_1823054225"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#9d5e19"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#053d8b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#1164b0" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#298bc8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#3fb7db" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook URI="img/Python-ML-III-book-cover.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="01. 赋予计算机从数据中学习的能力" POSITION="bottom_or_right" ID="ID_46276205" CREATED="1732708229823" MODIFIED="1732708314016"/>
+<node TEXT="02. 训练简单的机器学习分类算法" POSITION="bottom_or_right" ID="ID_1559958652" CREATED="1732708244568" MODIFIED="1732708315382"/>
+<node TEXT="03. scikit-learn机器学习分类器" POSITION="bottom_or_right" ID="ID_667278910" CREATED="1732708262714" MODIFIED="1732708317002"/>
+<node TEXT="04. 构建良好的训练数据集--数据预处理" POSITION="bottom_or_right" ID="ID_1611020259" CREATED="1732708273693" MODIFIED="1732708319047"/>
+<node TEXT="05. 通过降维压缩数据" POSITION="bottom_or_right" ID="ID_631050398" CREATED="1732708295635" MODIFIED="1732708321142"/>
+<node TEXT="06. 模型评估和超参数调优的最佳实践" POSITION="bottom_or_right" ID="ID_137727950" CREATED="1732708321327" MODIFIED="1732708338982"/>
+<node TEXT="07. 组合不同模型的集成学习" POSITION="bottom_or_right" ID="ID_1048989968" CREATED="1732708339402" MODIFIED="1732708349939"/>
+<node TEXT="08. 用机器学习进行情感分析" POSITION="bottom_or_right" ID="ID_814217610" CREATED="1732708353617" MODIFIED="1732708363743"/>
+<node TEXT="09. 将机器学习模型嵌入Web应用" POSITION="bottom_or_right" ID="ID_1668447794" CREATED="1732708363906" MODIFIED="1732708375844"/>
+<node TEXT="10. 用回归分析预测连续目标变量" POSITION="bottom_or_right" ID="ID_1147655962" CREATED="1732708378897" MODIFIED="1732708392612"/>
+<node TEXT="11. 用聚类分析处理无标签数据" POSITION="bottom_or_right" ID="ID_1312542386" CREATED="1732708393088" MODIFIED="1732708414353"/>
+<node TEXT="12. 从零开始实现多层人工神经网络" POSITION="bottom_or_right" ID="ID_890619876" CREATED="1732708414510" MODIFIED="1732708427803"/>
+<node TEXT="13. 同TensorFlow并行训练神经网络" POSITION="bottom_or_right" ID="ID_851242862" CREATED="1732708427998" MODIFIED="1732708443192"/>
+<node TEXT="14. 深入探讨TensorFlow的工作原理" POSITION="bottom_or_right" ID="ID_12272725" CREATED="1732708443349" MODIFIED="1732708454184"/>
+<node TEXT="15. 用深度卷积神经网络为图像分类" POSITION="bottom_or_right" ID="ID_1289200082" CREATED="1732708457101" MODIFIED="1732708474911"/>
+<node TEXT="16. 用循环神经网络为序列数据建模" POSITION="bottom_or_right" ID="ID_589744119" CREATED="1732708477679" MODIFIED="1732708490096"/>
+<node TEXT="17. 用生成对抗网络GAN合成新数据" POSITION="bottom_or_right" ID="ID_1508473265" CREATED="1732708490747" MODIFIED="1732708606664"/>
+<node TEXT="18. 用于复杂环境决策的强化学习" POSITION="bottom_or_right" ID="ID_991169321" CREATED="1732708508847" MODIFIED="1732708523932"/>
+<node TEXT="Experts" POSITION="top_or_left" ID="ID_1737560980" CREATED="1732709112453" MODIFIED="1732709119343">
+<node TEXT="Geoffrey E. Hinton" ID="ID_633709529" CREATED="1732709121195" MODIFIED="1732709148933" LINK="https://www.cs.toronto.edu/~hinton/"/>
+<node TEXT="Andrew Ng" ID="ID_791654982" CREATED="1732709199963" MODIFIED="1732709206121" LINK="http://www.andrewng.org/"/>
+<node TEXT="Yann Lecun" ID="ID_1893355121" CREATED="1732709242858" MODIFIED="1732709276254" LINK="http://yann.lecun.com"/>
+<node TEXT="Juergen Schmidhuber" ID="ID_266496868" CREATED="1732709246848" MODIFIED="1732709252488"/>
+<node TEXT="Yoshua Bengio" ID="ID_934246764" CREATED="1732709252669" MODIFIED="1732709257620"/>
+</node>
+<node TEXT="Python Library" POSITION="top_or_left" ID="ID_1473394425" CREATED="1732709097889" MODIFIED="1732709107334">
+<node TEXT="scikit-learn" ID="ID_626396898" CREATED="1732708644596" MODIFIED="1732708654689"/>
+<node TEXT="Keras" ID="ID_829598764" CREATED="1732708655075" MODIFIED="1732708657622"/>
+<node TEXT="TensorFlow" ID="ID_613646618" CREATED="1732708657795" MODIFIED="1732708660096"/>
+</node>
+</node>
+</map>
