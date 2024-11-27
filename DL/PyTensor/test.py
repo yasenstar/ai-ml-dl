a9d8b2275dd7d38a7690b1fe036d4736a96fae81
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-27 19:02:11 +0800

    Update test.py

--- DL/PyTensor/test.py
+++ DL/PyTensor/test.py
@@ -1,2 +1,9 @@
-# from pytensor import *
+import numpy
 import pytensor.tensor as pt
+from pytensor import function
+x = pt.dscalar('x')
+y = pt.dscalar('y')
+z = x + y
+print(z)
+f = function([x,y],z)
+print(f(2,3))
