c6c2e3891dcc145dcc5283b19003eb0fb12f5fc0
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-27 15:03:27 +0800

    prepare pytensor

---
+++ DL/PyTensor.mm
@@ -0,0 +1,126 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="PyTensor" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1732688338571" LINK="https://pytensor.readthedocs.io/en/latest/index.html">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#fdf6e3ff" zoom="0.9090909">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_solarized_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1704360557" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1704360557" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1083719472" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1083719472"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#fdf6e3">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook URI="img/pyTensor.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="Getting Started" POSITION="bottom_or_right" ID="ID_648788938" CREATED="1732688291296" MODIFIED="1732688295035">
+<node TEXT="001 Opening" ID="ID_1609693353" CREATED="1732688345334" MODIFIED="1732688358528">
+<icon BUILTIN="video"/>
+</node>
+<node TEXT="PyTensor is a Python library that allows one to define, optimize/rewrite, and evaluate mathematical expressions, especially ones involving multi-dimensional arrays" ID="ID_681887309" CREATED="1732688378413" MODIFIED="1732688382702"/>
+<node TEXT="The library that PyTensor is based on, Theano, was written at the LISA lab to support rapid development of efficient machine learning algorithms but while Theano was commonly referred to as a “deep learning” (DL) library, PyTensor is not a DL library" FOLDED="true" ID="ID_1404377986" CREATED="1732688405370" MODIFIED="1732688408518">
+<node TEXT="Theano" ID="ID_402128264" CREATED="1732687531886" MODIFIED="1732687543386" LINK="https://github.com/Theano/Theano">
+<node TEXT="MILA has stopped developing Theano: https://groups.google.com/d/msg/theano-users/7Poq8BZutbY/rNCIfvAEAwAJ&#xa;&#xa;The PyMC developers have forked Theano to a new project called PyTensor that is being actively developed: https://github.com/pymc-devs/pytensor" ID="ID_927421662" CREATED="1732687550645" MODIFIED="1732687557328"/>
+</node>
+</node>
+<node TEXT="If numpy is to be compared to MATLAB and sympy to Mathematica, PyTensor is a sort of hybrid of the two which tries to combine the best of both worlds" ID="ID_326280933" CREATED="1732688478736" MODIFIED="1732688482395"/>
+</node>
+<node TEXT="Installing PyTensor" FOLDED="true" POSITION="bottom_or_right" ID="ID_438660426" CREATED="1732688295194" MODIFIED="1732688299672">
+<node TEXT="002 Installation" ID="ID_1053447714" CREATED="1732688768690" MODIFIED="1732688774385"/>
+</node>
+<node TEXT="Tutorial" FOLDED="true" POSITION="bottom_or_right" ID="ID_1230154352" CREATED="1732688299842" MODIFIED="1732688302292">
+<node TEXT="01 Baby Steps - Algebra" ID="ID_64590896" CREATED="1732688594023" MODIFIED="1732688602711"/>
+<node TEXT="02 More Examples" ID="ID_1854936960" CREATED="1732688602886" MODIFIED="1732688607600"/>
+<node TEXT="03 Derivatives in PyTensor" ID="ID_1089621236" CREATED="1732688607774" MODIFIED="1732688614831"/>
+<node TEXT="04 Conditions" ID="ID_1256098931" CREATED="1732688614998" MODIFIED="1732688618688"/>
+<node TEXT="05 Loop" ID="ID_219594239" CREATED="1732688618918" MODIFIED="1732688621775"/>
+<node TEXT="06 Handle Shape Information" ID="ID_1032425793" CREATED="1732688621967" MODIFIED="1732688648879"/>
+<node TEXT="07 Broadcasting" ID="ID_840727596" CREATED="1732688633710" MODIFIED="1732688654583"/>
+<node TEXT="08 Sparse" ID="ID_1222435533" CREATED="1732688654740" MODIFIED="1732688657421"/>
+<node TEXT="09 Generate Pseudo Random Number" ID="ID_1656570491" CREATED="1732688657588" MODIFIED="1732688674469"/>
+<node TEXT="10 Configuration Settings and Compiling Modes" ID="ID_1317475804" CREATED="1732688674715" MODIFIED="1732688689107"/>
+<node TEXT="11 Printing/Drawing PyTensor Graphs" ID="ID_1205175529" CREATED="1732688689278" MODIFIED="1732688704721"/>
+<node TEXT="12 Debugging PyTensor" ID="ID_557765942" CREATED="1732688704902" MODIFIED="1732688712775"/>
+<node TEXT="13 Dealing with NaNs" ID="ID_1904303966" CREATED="1732688712955" MODIFIED="1732688720503"/>
+<node TEXT="14 Profiling PyTensor Function" ID="ID_1467268934" CREATED="1732688720685" MODIFIED="1732688728696"/>
+<node TEXT="15 Loading and Saving" ID="ID_1408609764" CREATED="1732688728862" MODIFIED="1732688733814"/>
+<node TEXT="16 Understanding Memory Aliasing for Speed and Correctness" ID="ID_1369268507" CREATED="1732688733998" MODIFIED="1732688749446"/>
+<node TEXT="17 Multi Cores Support" ID="ID_496183056" CREATED="1732688749620" MODIFIED="1732688756856"/>
+</node>
+</node>
+</map>
