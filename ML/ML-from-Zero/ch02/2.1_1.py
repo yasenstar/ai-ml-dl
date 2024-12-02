affd43fa25615def23dd0763f1150b5701f6fa69
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-12-02 19:08:36 +0800

    add 2.1.2

---
+++ ML/ML-from-Zero/ch02/2.1_1.py
@@ -0,0 +1,8 @@
+# 二维空间中的向量外积
+
+import numpy as np
+a = np.array([1,2])
+b = np.array([3,4])
+print(np.cross(a,b))
+print("*"*100)
+print(np.cross(b,a))
---
+++ ML/ML-from-Zero/ch02/2.1_2.py
@@ -0,0 +1,8 @@
+# 三维空间中的向量外积
+
+import numpy as np
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+print(np.cross(a,b))
+print("*"*100)
+print(np.cross(b,a))
