6e760751dd558daaedd56cb077ffe1bdef165f4a
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-02-01 12:50:43 +0800

    add AI mindmap

---
+++ AI/AI.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="AI" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1738385330381">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8ff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="AI Learning Sources" POSITION="top_or_left" ID="ID_278064645" CREATED="1738384578014" MODIFIED="1738385381656">
+<node TEXT="Intel: AI Courses and Certifications" POSITION="bottom_or_right" ID="ID_1206518938" CREATED="1738384428328" MODIFIED="1738384441648" LINK="https://www.intel.com/content/www/us/en/developer/topic-technology/artificial-intelligence/training/overview.html"/>
+<node TEXT="IBM: AI Foundations for Everyone" POSITION="bottom_or_right" ID="ID_400696519" CREATED="1738384502887" MODIFIED="1738384514628" LINK="https://www.coursera.org/specializations/ai-foundations-for-everyone"/>
+<node TEXT="Google: Google AI for Anyone" POSITION="bottom_or_right" ID="ID_709922885" CREATED="1738384547510" MODIFIED="1738384559473" LINK="https://www.edx.org/learn/artificial-intelligence/google-google-ai-for-anyone?index=product&amp;queryID=20f255a72e55f01f6dbb7ba10089489d&amp;position=3"/>
+<node TEXT="AWS Skill Builder" POSITION="bottom_or_right" ID="ID_606987253" CREATED="1738384572883" MODIFIED="1738384593348" LINK="https://aws.amazon.com/training/learn-about/machine-learning/"/>
+<node TEXT="University of Pennsylvania: AI for Business" POSITION="bottom_or_right" ID="ID_1776240576" CREATED="1738384456037" MODIFIED="1738384471966" LINK="https://www.coursera.org/specializations/ai-for-business-wharton"/>
+</node>
+<node TEXT="AI Certifications" POSITION="bottom_or_right" ID="ID_901847884" CREATED="1738385306558" MODIFIED="1738385390318">
+<node TEXT="01. Artificial Intelligence Fundamentals form IBM" POSITION="bottom_or_right" ID="ID_1242447844" CREATED="1738384045368" MODIFIED="1738384172129" LINK="https://skillsbuild.org/adult-learners/explore-learning/artificial-intelligence"/>
+<node TEXT="02. Big Data, AI and Athics from UC Davis (Coursera)" POSITION="bottom_or_right" ID="ID_1976490417" CREATED="1738384076959" MODIFIED="1738384168549" LINK="https://www.coursera.org/learn/big-data-ai-ethics"/>
+<node TEXT="03. GenAI for Executives &amp; Business Leaders by IBM" POSITION="bottom_or_right" ID="ID_1582957781" CREATED="1738384145183" MODIFIED="1738384180921" LINK="https://www.coursera.org/learn/generative-ai-for-executives-business-leaders-introduction"/>
+<node TEXT="04. Googles Introduction to Generative AI" POSITION="bottom_or_right" ID="ID_738923409" CREATED="1738384187527" MODIFIED="1738384218378" LINK="https://www.cloudskillsboost.google/paths/118"/>
+<node TEXT="05. Generative AI for Educators from Google &amp; MIT" POSITION="bottom_or_right" ID="ID_1946447782" CREATED="1738384201163" MODIFIED="1738384254171" LINK="https://grow.google/intl/en_ca/ai-for-educators/"/>
+<node TEXT="06. GenAI for Software Developoment by Deep Learning" POSITION="bottom_or_right" ID="ID_889140950" CREATED="1738384236912" MODIFIED="1738384280638" LINK="https://www.deeplearning.ai/courses/generative-ai-for-software-development/"/>
+<node TEXT="07. AI for Good Specialization by Deep Learning" POSITION="bottom_or_right" ID="ID_1879753951" CREATED="1738384265319" MODIFIED="1738384321760" LINK="https://www.deeplearning.ai/courses/ai-for-good/"/>
+<node TEXT="08. Intro to AI with Python from Harward" POSITION="bottom_or_right" ID="ID_237397449" CREATED="1738384288362" MODIFIED="1738384400346" LINK="https://pll.harvard.edu/course/cs50-introduction-computer-science?delta=0"/>
+<node TEXT="09. Drive Productivity with Salesforce AI" POSITION="bottom_or_right" ID="ID_294843103" CREATED="1738384327950" MODIFIED="1738384659967" LINK="https://trailhead.salesforce.com/content/learn/trails/drive-productivity-with-einstein-ai"/>
+<node TEXT="10. Prompt Engineering with ChatGPT from ASU (Coursera)" POSITION="bottom_or_right" ID="ID_1696907333" CREATED="1738384635560" MODIFIED="1738384701886" LINK="https://www.coursera.org/learn/ai-foundations-prompt-engineering-with-chatgpt"/>
+<node TEXT="11. Generative AI for Legal Services by Vanderbilt U (Coursera)" POSITION="bottom_or_right" ID="ID_1982364136" CREATED="1738384666891" MODIFIED="1738384739532" LINK="https://www.coursera.org/learn/generative-ai-for-legal-services-primer"/>
+<node TEXT="12. Generative AI in Marketing by UVA (Coursera)" POSITION="bottom_or_right" ID="ID_1966113680" CREATED="1738384710093" MODIFIED="1738384838542" LINK="https://www.coursera.org/specializations/generative-ai-in-marketing"/>
+<node TEXT="13. Coding with Generative AI by Fractal (Coursera)" POSITION="bottom_or_right" ID="ID_1906066059" CREATED="1738384813908" MODIFIED="1738384873318" LINK="https://www.coursera.org/learn/introduction-coding-with-generative-ai"/>
+<node TEXT="14. Elements of AI" POSITION="bottom_or_right" ID="ID_3360608" CREATED="1738385277958" MODIFIED="1738385292989" LINK="https://course.elementsofai.com/"/>
+</node>
+</node>
+</map>
