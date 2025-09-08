4ad09f35f5b085bab2c96ab66956c72321062f56
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-09-08 11:06:28 +0800

    Create Intro to AI in Azure (AI-900T00-A).mm

---
+++ AI/Intro to AI in Azure (AI-900T00-A).mm
@@ -0,0 +1,210 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Intro to AI in Azure&#xa;(AI-900T00-A)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1757297636972"><hook NAME="MapStyle" background="#f5f5dcff" zoom="0.82644624">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_sepia_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.6 pt;-10.2 pt;" ENDINCLINATION="81.6 pt;19.8 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_411331447" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_411331447"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#2c2b29" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="01 Introduction to AI Concepts" FOLDED="true" POSITION="bottom_or_right" ID="ID_915434132" CREATED="1757297682203" MODIFIED="1757297689534">
+<node TEXT="Generative AI" ID="ID_248908327" CREATED="1757298317030" MODIFIED="1757298321992">
+<node TEXT="Generative AI is a branch of AI that enables software applications to generate new content; often natural language dialogs, but also images, video, code, and other formats" POSITION="bottom_or_right" ID="ID_1208447485" CREATED="1757298187159" MODIFIED="1757298267015"/>
+<node POSITION="bottom_or_right" ID="ID_823784668" CREATED="1757298325606" MODIFIED="1757298381700"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The ability to generate content is base on <span style="font-weight: bold;">language model</span>, which has been trained with huge volumes of data - often documents from the Internet or other public sources of information.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Computer Vision" ID="ID_293327512" CREATED="1757298397669" MODIFIED="1757298405573">
+<node TEXT="Computer vision is accomplished by using large numbers of images to train a model" ID="ID_702152265" CREATED="1757298405576" MODIFIED="1757298429287"/>
+</node>
+<node TEXT="Speech" ID="ID_1978003077" CREATED="1757298587233" MODIFIED="1757298589682">
+<node TEXT="Speech recognition is the ability to AI to &quot;hear&quot; and interpret speech" ID="ID_549040117" CREATED="1757298589684" MODIFIED="1757298612243"/>
+<node TEXT="Speech synthesis is the ability of AI to vocalize words as spoken language" ID="ID_205067897" CREATED="1757298612466" MODIFIED="1757298674627"/>
+</node>
+<node TEXT="Natural Language Processing" ID="ID_1584443744" CREATED="1757298693824" MODIFIED="1757298699367">
+<node TEXT="NLP capabilities are based on models that are trained to do particular types of text analysis" ID="ID_174037702" CREATED="1757298778352" MODIFIED="1757298810114"/>
+<node TEXT="Common NLP tasks" ID="ID_1689032601" CREATED="1757298818463" MODIFIED="1757298824224">
+<node TEXT="Entity extraction: identifying mentions of entities like people, places, organization in a document" ID="ID_1609859423" CREATED="1757298824226" MODIFIED="1757298854338"/>
+<node TEXT="Text classification: assigning document to a specific category" ID="ID_1515156582" CREATED="1757298854495" MODIFIED="1757298907411"/>
+<node TEXT="Sentiment (情绪) analysis: determining whether a body of text is positive, negative, or neutral and inferring opinions" ID="ID_1989354644" CREATED="1757298907568" MODIFIED="1757298987812"/>
+<node TEXT="Language detection: identifying the language in which text is written." ID="ID_71000997" CREATED="1757298933151" MODIFIED="1757298954193"/>
+</node>
+</node>
+<node TEXT="Extract data and Insights" ID="ID_610063566" CREATED="1757298699505" MODIFIED="1757298705571">
+<node TEXT="The basis for most document analysis solutions is a computer vision technology called Optical Character Recognition (OCR)" ID="ID_880586143" CREATED="1757298989070" MODIFIED="1757299018951"/>
+</node>
+<node TEXT="Responsibile AI" ID="ID_1183338680" CREATED="1757298705745" MODIFIED="1757298709270">
+<node TEXT="Fairness" ID="ID_605526185" CREATED="1757299160479" MODIFIED="1757299170738"/>
+<node TEXT="Reliability and Safety" ID="ID_658137588" CREATED="1757299175556" MODIFIED="1757299182593"/>
+<node TEXT="Privacy and Security" ID="ID_164270191" CREATED="1757299182767" MODIFIED="1757299186114"/>
+<node TEXT="Inclusiveness" ID="ID_1169495414" CREATED="1757299186286" MODIFIED="1757299190896"/>
+<node TEXT="Transparency" ID="ID_859797565" CREATED="1757299193839" MODIFIED="1757299197153"/>
+<node TEXT="Accountability" ID="ID_671032439" CREATED="1757299197343" MODIFIED="1757299202705"/>
+</node>
+</node>
+<node TEXT="02 Introduction to Machine Learning Concepts" POSITION="bottom_or_right" ID="ID_350938948" CREATED="1757297689692" MODIFIED="1757297696894">
+<node TEXT="What is Machine Learning?" ID="ID_954029174" CREATED="1757299430845" MODIFIED="1757299438447">
+<node TEXT="Use data from past observations to predict unknown outcomes or values" ID="ID_1336124838" CREATED="1757299965400" MODIFIED="1757299982459"/>
+<node TEXT="Machine Learning as a function" ID="ID_337715254" CREATED="1757300155799" MODIFIED="1757300164375">
+<node ID="ID_1024881406" CREATED="1757300164377" MODIFIED="1757300206893"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A machine learning model is a software application that encapsulates a <span style="font-weight: bold; font-style: italic;">function</span><span style="font-weight: normal;">&#xa0;&#xa0;to calculate an output value based on one or more input values</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1126334280" CREATED="1757300207063" MODIFIED="1757300218138"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The process of defining that function is known as <span style="font-weight: bold; font-style: italic;">training</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_524409953" CREATED="1757300219432" MODIFIED="1757300246510"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      After the function has been defined, you can use it to predict new value in a process called <span style="font-weight: bold; font-style: italic;">inferencing</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Type of Machine Learning" ID="ID_1284901068" CREATED="1757299438621" MODIFIED="1757299443726">
+<node TEXT="Supervised Machine Learning" ID="ID_306263126" CREATED="1757300637603" MODIFIED="1757300644965">
+<node TEXT="Regression" ID="ID_916582184" CREATED="1757300652531" MODIFIED="1757300655862"/>
+<node TEXT="Classification" ID="ID_191610558" CREATED="1757300656019" MODIFIED="1757300658259">
+<node TEXT="Binary Classificatin" ID="ID_1216614277" CREATED="1757300658261" MODIFIED="1757300663334"/>
+<node TEXT="Multiclass classfication" ID="ID_1248864379" CREATED="1757300663475" MODIFIED="1757300674533"/>
+</node>
+</node>
+<node TEXT="Unsupervised Machine Learning" ID="ID_1866231587" CREATED="1757300645140" MODIFIED="1757300651831">
+<node TEXT="Clustering" ID="ID_1382147402" CREATED="1757300675923" MODIFIED="1757300678999"/>
+</node>
+</node>
+<node TEXT="Regression" ID="ID_1872245652" CREATED="1757299443886" MODIFIED="1757299446559">
+<node TEXT="A form of supervised machine learning in which the label represents a categorization, or class" ID="ID_352489256" CREATED="1757300719986" MODIFIED="1757300747271"/>
+</node>
+<node TEXT="Binary Classification" ID="ID_1731270859" CREATED="1757299446733" MODIFIED="1757299451870"/>
+<node TEXT="Multiclass Classification" ID="ID_1717247090" CREATED="1757299452029" MODIFIED="1757299463600"/>
+<node TEXT="Clustering" ID="ID_1527378885" CREATED="1757299463757" MODIFIED="1757299465614"/>
+<node TEXT="Deep Learning" ID="ID_1683443449" CREATED="1757299465773" MODIFIED="1757299468335"/>
+<node TEXT="Transformers" ID="ID_692303129" CREATED="1757299468493" MODIFIED="1757299470878"/>
+</node>
+<node TEXT="03 Get Started with Machine Learning in Azure" POSITION="bottom_or_right" ID="ID_471762137" CREATED="1757297697083" MODIFIED="1757297714908"/>
+<node TEXT="04 Introduction to Generative AI Concepts" POSITION="bottom_or_right" ID="ID_830422575" CREATED="1757297715067" MODIFIED="1757297722030"/>
+<node TEXT="05 Get Started with Generative AI in Azure" POSITION="bottom_or_right" ID="ID_1812370621" CREATED="1757297722317" MODIFIED="1757297730348"/>
+<node TEXT="06 Introduction to Natural Language Processing Concepts" POSITION="bottom_or_right" ID="ID_391858252" CREATED="1757297735035" MODIFIED="1757297748942"/>
+<node TEXT="07 Get Started with Natural Language Processing in Azure" POSITION="bottom_or_right" ID="ID_511199696" CREATED="1757297749099" MODIFIED="1757297763406"/>
+<node TEXT="08 Get Started with Speech in Azure" POSITION="bottom_or_right" ID="ID_1027181676" CREATED="1757297763562" MODIFIED="1757297772126"/>
+<node TEXT="09 Introduction to Computer Vision Concepts" POSITION="bottom_or_right" ID="ID_1967765588" CREATED="1757297772282" MODIFIED="1757297781548"/>
+<node TEXT="10 Get Started with Computer Vision in Azure" POSITION="bottom_or_right" ID="ID_527542833" CREATED="1757297781722" MODIFIED="1757297798892"/>
+<node TEXT="11 Introduction to AI-Powered Information Extraction Concepts" POSITION="bottom_or_right" ID="ID_1842870999" CREATED="1757297799069" MODIFIED="1757297814971"/>
+<node TEXT="12 Get Started with AI-Powered Information Extraction in Azure" POSITION="bottom_or_right" ID="ID_453784311" CREATED="1757297815147" MODIFIED="1757297831692"/>
+</node>
+</map>
