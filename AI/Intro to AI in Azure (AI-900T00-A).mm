54e8620decc5097c18051d777476b1f14b2614bd
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-09-13 10:56:55 +0800

    Update Intro to AI in Azure (AI-900T00-A).mm

--- AI/Intro to AI in Azure (AI-900T00-A).mm
+++ AI/Intro to AI in Azure (AI-900T00-A).mm
@@ -190,7 +190,7 @@
 <node TEXT="Regression" ID="ID_1872245652" CREATED="1757299443886" MODIFIED="1757299446559">
 <node TEXT="A form of supervised machine learning in which the label represents a categorization, or class" ID="ID_352489256" CREATED="1757300719986" MODIFIED="1757300747271"/>
 <node TEXT="Regression models are trained to predict numeric label values based on training data that includes both features and known labels." ID="ID_657570072" CREATED="1757301069778" MODIFIED="1757301102533"/>
-<node TEXT="Four key elements in Regression of the training process" ID="ID_1883100874" CREATED="1757301173986" MODIFIED="1757301191472">
+<node TEXT="Four key elements in Regression of the training process" FOLDED="true" ID="ID_1883100874" CREATED="1757301173986" MODIFIED="1757301191472">
 <node TEXT="1. Split the training data (randomly) to create a dataset with which to train the model while holding back a subset of the data that you&apos;ll use to validate the trained model" ID="ID_740262162" CREATED="1757301191475" MODIFIED="1757301235235"/>
 <node TEXT="2. Use an algorithm to fit the training data to a model. (e.g. linear regression)" ID="ID_1923032471" CREATED="1757301235376" MODIFIED="1757301263346"/>
 <node TEXT="3. Use the validation data you held back to test the model by predicting labels for the features" ID="ID_131695275" CREATED="1757301263520" MODIFIED="1757301287716"/>
@@ -209,14 +209,126 @@
 </richcontent>
 </node>
 </node>
+<node TEXT="Regression Evaluation Metrics" ID="ID_607730436" CREATED="1757301590991" MODIFIED="1757301597759">
+<node TEXT="Mean Absolute Error (MAE)" ID="ID_232976688" CREATED="1757301597762" MODIFIED="1757301641747"/>
+<node TEXT="Mean Squared Error (MSE)" ID="ID_107160677" CREATED="1757301642304" MODIFIED="1757301649889"/>
+<node TEXT="Root Mean Squared Error (RMSE)" ID="ID_1436233542" CREATED="1757301693135" MODIFIED="1757301701954"/>
+<node TEXT="Coefficient of Determination (R²)" ID="ID_106396647" CREATED="1757301704785" MODIFIED="1757303446516"/>
 </node>
-<node TEXT="Binary Classification" ID="ID_1731270859" CREATED="1757299446733" MODIFIED="1757299451870"/>
-<node TEXT="Multiclass Classification" ID="ID_1717247090" CREATED="1757299452029" MODIFIED="1757299463600"/>
+<node TEXT="Iterative Training 迭代训练" ID="ID_1504858543" CREATED="1757303448850" MODIFIED="1757303468258">
+<node TEXT="Feature selection and preparation" ID="ID_971512107" CREATED="1757303468261" MODIFIED="1757303498675"/>
+<node TEXT="Algorithm selection" ID="ID_1668388135" CREATED="1757303498849" MODIFIED="1757303503348"/>
+<node TEXT="Algorithm parameters" ID="ID_809253424" CREATED="1757303503508" MODIFIED="1757303508629"/>
+</node>
+</node>
+<node TEXT="Binary Classification" ID="ID_1731270859" CREATED="1757299446733" MODIFIED="1757299451870">
+<node ID="ID_1472416660" CREATED="1757303562640" MODIFIED="1757303631171"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Instead of calculating number values like a regression model, the algorithms used to train classification models calculate <span style="font-weight: bold; font-style: italic;">probability</span><span style="font-weight: normal;">&#xa0;&#xa0;values for class assignment and the evaluation metrics used to assess model performance compare the predicted classes to the actual data</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_315583722" CREATED="1757303705616" MODIFIED="1757303743748"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Despite its name, in machine learning <span style="font-weight: bold; font-style: italic;">logistic regression</span><span style="font-weight: normal;">&#xa0;is used for classification, not regression.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Binary Classification evaluation: confusion matrix: (TN+TP)/(TN+FN+FP+TP)" ID="ID_1846127504" CREATED="1757304080157" MODIFIED="1757304115486">
+<node TEXT="Recall - also called &quot;true posive rate (TPR)&quot; - is a metric measures the proportion of positive cases that the model identified correctly. (TP/(TP+FN))" POSITION="bottom_or_right" ID="ID_390023590" CREATED="1757304135390" MODIFIED="1757304481230"/>
+<node TEXT="False positive rate (FPR): (FP/(FP+TN))" POSITION="bottom_or_right" ID="ID_176363940" CREATED="1757304454602" MODIFIED="1757304477024"/>
+<node TEXT="Precision is a metric measures the proportion of predicted positive cases where the true label is actually positive. (TP/(TP+FP))" POSITION="bottom_or_right" ID="ID_512162164" CREATED="1757304278876" MODIFIED="1757304479685"/>
+<node TEXT="F1-score: overall metric combines recall and precision.&#xa;(2 x Precion x Recall) / (Precision + Recall)" POSITION="bottom_or_right" ID="ID_1069586510" CREATED="1757304339691" MODIFIED="1757304378448"/>
+<node TEXT="Area Under the Curve (AUC)" POSITION="bottom_or_right" ID="ID_376031576" CREATED="1757304386492" MODIFIED="1757304408510"/>
+</node>
+</node>
+<node TEXT="Multiclass Classification" ID="ID_1717247090" CREATED="1757299452029" MODIFIED="1757299463600">
+<node TEXT="Two kinds of algorithms" ID="ID_640832717" CREATED="1757304583066" MODIFIED="1757304591866">
+<node TEXT="One-vs-Rest (OvR) algorithms" ID="ID_944812473" CREATED="1757304591869" MODIFIED="1757304599946"/>
+<node TEXT="Multinomial algorithms" ID="ID_573136783" CREATED="1757304600186" MODIFIED="1757304607469"/>
+</node>
+<node TEXT="You can evaluate a multiclass classifier by calculating binary classification metrics for each individual class" ID="ID_1241565156" CREATED="1757304663082" MODIFIED="1757304692734"/>
+</node>
 <node TEXT="Clustering" ID="ID_1527378885" CREATED="1757299463757" MODIFIED="1757299465614">
 <node TEXT="A clustering algorithm identifies similarities between observations base on their features, and groups them into discrete clusters." ID="ID_1908001043" CREATED="1757300934288" MODIFIED="1757300972436"/>
+<node TEXT="Evaluation metrics" ID="ID_637973052" CREATED="1757304873913" MODIFIED="1757304882009">
+<node TEXT="Average distance to cluster center" ID="ID_1393552384" CREATED="1757304882012" MODIFIED="1757304890844"/>
+<node TEXT="Average distance to other center" ID="ID_504549870" CREATED="1757304891016" MODIFIED="1757304896363"/>
+<node TEXT="Maximum distance to cluster center" ID="ID_1966951727" CREATED="1757304896537" MODIFIED="1757304902828"/>
+<node TEXT="Silhoustte" ID="ID_1508177899" CREATED="1757304903577" MODIFIED="1757304910476"/>
 </node>
-<node TEXT="Deep Learning" ID="ID_1683443449" CREATED="1757299465773" MODIFIED="1757299468335"/>
-<node TEXT="Transformers" ID="ID_692303129" CREATED="1757299468493" MODIFIED="1757299470878"/>
+</node>
+<node TEXT="Deep Learning" ID="ID_1683443449" CREATED="1757299465773" MODIFIED="1757299468335">
+<node TEXT="Deep Learning is an advanced form of machine learning that tries to emulate the way the human brain learns" ID="ID_430564566" CREATED="1757304944297" MODIFIED="1757304973754"/>
+<node TEXT="The key to deep learning is the creation of an artificial neural network that simulates electrochemical activity in biological neurons by using mathematical functions" ID="ID_870903556" CREATED="1757304973977" MODIFIED="1757305066430"/>
+</node>
+<node TEXT="Transformers" ID="ID_692303129" CREATED="1757299468493" MODIFIED="1757299470878">
+<node ID="ID_847919895" CREATED="1757305643173" MODIFIED="1757305669800"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Today's cutting-edge large language models are based on the <span style="font-weight: bold; font-style: italic;">transformer</span><span style="font-weight: normal;">&#xa0;&#xa0;architecture</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Transformer models are trained with large volumes of text, enabling them to represent the semantic relationships between words and use those relationships to determine probable sequences of text that make sense" ID="ID_87487438" CREATED="1757305710357" MODIFIED="1757305787957"/>
+<node TEXT="Two components in Transformer Model Architecture" ID="ID_1866503034" CREATED="1757305800100" MODIFIED="1757305814019">
+<node ID="ID_1102800061" CREATED="1757305814021" MODIFIED="1757305838072"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      An <span style="font-weight: bold; font-style: italic;">encoder</span><span style="font-weight: normal;">&#xa0;&#xa0;block that creates semantic representations of the training vocabulary</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_871647778" CREATED="1757305838211" MODIFIED="1757305851894"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A <span style="font-weight: bold; font-style: italic;">decoder</span><span style="font-weight: normal;">&#xa0;&#xa0;block that generates new language sequences</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Practical Implementation" ID="ID_1739530254" CREATED="1757305982433" MODIFIED="1757305993185">
+<node TEXT="Bidirectional Encoder Representations from Transformer (BERT), developed by Google, uses only the encoder block" ID="ID_1356988273" CREATED="1757305993188" MODIFIED="1757306037844"/>
+<node TEXT="Generative Pretrained Transform (GPT) developed by OpenAI uses only the decoder block" ID="ID_1754960434" CREATED="1757306038320" MODIFIED="1757306091126"/>
+</node>
+</node>
 </node>
 <node TEXT="03 Get Started with Machine Learning in Azure" POSITION="bottom_or_right" ID="ID_471762137" CREATED="1757297697083" MODIFIED="1757297714908"/>
 <node TEXT="04 Introduction to Generative AI Concepts" POSITION="bottom_or_right" ID="ID_830422575" CREATED="1757297715067" MODIFIED="1757297722030"/>
