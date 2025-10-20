50d45b2fb9d12214e25638b1a203c564c49d7715
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-10-20 11:09:02 -0400

    learning module1

--- AI/AI_Fundamentals/AI_Fundamentals.mm
+++ AI/AI_Fundamentals/AI_Fundamentals.mm
@@ -82,7 +82,207 @@
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <font BOLD="true"/>
 <node TEXT="0. Credly Account Info" POSITION="bottom_or_right" ID="ID_695547037" CREATED="1760922188399" MODIFIED="1760922199834"/>
-<node TEXT="1. Intro to AI" POSITION="bottom_or_right" ID="ID_1428386737" CREATED="1760922090495" MODIFIED="1760922097575"/>
+<node TEXT="1. Intro to AI" POSITION="bottom_or_right" ID="ID_1428386737" CREATED="1760922090495" MODIFIED="1760969153258"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Learning Points:
+    </p>
+    <ul>
+      <li>
+        Define artificial intelligence
+      </li>
+      <li>
+        Describe three levels of artificial intelligence
+      </li>
+      <li>
+        Describe the history of AI from the past to the possible future
+      </li>
+      <li>
+        Define and describe machine learning
+      </li>
+      <li>
+        Differentiate between structured and unstructured data
+      </li>
+      <li>
+        Describe how machine learning structures data
+      </li>
+      <li>
+        Describe how machine learning structures unstructured data
+      </li>
+      <li>
+        Describe how machine learning uses probabilistic calculation to solve problems
+      </li>
+      <li>
+        Describe three methods by which machine learning analyzes data
+      </li>
+      <li>
+        Describe an ideal relationship between humans and machine learning
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node TEXT="1.0 Course Overview" FOLDED="true" ID="ID_915603208" CREATED="1760969158151" MODIFIED="1760969172631">
+<node TEXT="About this Course" ID="ID_642696191" CREATED="1760969314077" MODIFIED="1760969792299"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      After completing this course, you should be able to:
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        Define artificial intelligence
+      </li>
+      <li>
+        Differentiate between AI and augmented intelligence
+      </li>
+      <li>
+        Describe three levels of artificial intelligence
+      </li>
+      <li>
+        Describe the history of AI, from the past to the possible future
+      </li>
+      <li>
+        Define and describe machine learning
+      </li>
+      <li>
+        Differentiate between structured, unstructured, and semi-structured data
+      </li>
+      <li>
+        Describe how machine learning structures unstructured data
+      </li>
+      <li>
+        Describe how machine learning uses probabilistic calculation to solve problems
+      </li>
+      <li>
+        Describe three methods by which machine learning analyzes data
+      </li>
+      <li>
+        Describe an ideal relationship between humans and machine learning
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Earn a Credential!" ID="ID_1238036164" CREATED="1760969319066" MODIFIED="1760969325891"/>
+<node TEXT="Course Design" ID="ID_375727444" CREATED="1760969326203" MODIFIED="1760969328474"/>
+</node>
+<node TEXT="1.1 Module 1: What is AI?" ID="ID_566277613" CREATED="1760969165737" MODIFIED="1760969182803">
+<node TEXT="About this Module" FOLDED="true" ID="ID_706653476" CREATED="1760969330137" MODIFIED="1760969815191">
+<node TEXT="Three levels of predictions that AI can make" ID="ID_195308952" CREATED="1760969815949" MODIFIED="1760969823541">
+<node TEXT="Narrow" ID="ID_1738208716" CREATED="1760969823546" MODIFIED="1760969826006"/>
+<node TEXT="Broad" ID="ID_845376585" CREATED="1760969831227" MODIFIED="1760969832873"/>
+<node TEXT="General" ID="ID_138279982" CREATED="1760969826325" MODIFIED="1760969829402"/>
+</node>
+</node>
+<node TEXT="What is AI?" ID="ID_211660993" CREATED="1760969334476" MODIFIED="1760969336990">
+<node TEXT="Artificial Intelligence (AI) refers to the ability of a machine to learn patterns and make predictions." ID="ID_431388949" CREATED="1760969974685" MODIFIED="1760970003943"/>
+<node TEXT="AI does not replace human decisions; instead, AI add values to human judgment." ID="ID_1500073935" CREATED="1760970004105" MODIFIED="1760970021086"/>
+<node ID="ID_726335580" CREATED="1760970030922" MODIFIED="1760970063552"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      AI is a field that combines <span style="font-weight: bold;">computer science</span><span style="font-weight: normal;">&#xa0;&#xa0;and robust </span><span style="font-weight: bold;">dataset</span><span style="font-weight: normal;">&#xa0;to enable problem-solving.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="What is the difference between AI and Augmented Intelligence?" ID="ID_218478784" CREATED="1760969337146" MODIFIED="1760969352166">
+<node TEXT="Augmented intelligence has a modest goal of helping humans with tasks that are not practical to do." ID="ID_348773010" CREATED="1760970222697" MODIFIED="1760970246652"/>
+<node TEXT="Artificial intelligence has a lofty goal of mimicking human thinking and processes." ID="ID_1821927278" CREATED="1760970254523" MODIFIED="1760970278666"/>
+<node TEXT="AI today is not mature enough to perform independent tasks such as diagnosing cancer." ID="ID_1087641436" CREATED="1760970289402" MODIFIED="1760970310089"/>
+<node TEXT="Machines vs. Humans" ID="ID_235114939" CREATED="1760970590455" MODIFIED="1760970599466">
+<node TEXT="Machines" ID="ID_1054347016" CREATED="1760970599471" MODIFIED="1760970603274">
+<node TEXT="Ingesting Large Amount of Data" ID="ID_57466699" CREATED="1760970603278" MODIFIED="1760970610682"/>
+<node TEXT="Repetitive Tasks" ID="ID_1660829360" CREATED="1760970610850" MODIFIED="1760970614742"/>
+<node TEXT="Accurate" ID="ID_641134241" CREATED="1760970614952" MODIFIED="1760970617958"/>
+</node>
+<node TEXT="Humans" ID="ID_804104769" CREATED="1760970623841" MODIFIED="1760970626703">
+<node TEXT="Generalizing Information" ID="ID_525433817" CREATED="1760970629154" MODIFIED="1760970645043"/>
+<node TEXT="Creativity" ID="ID_76970903" CREATED="1760970648692" MODIFIED="1760970652313"/>
+<node TEXT="Emotional Intelligence" ID="ID_466054566" CREATED="1760970662923" MODIFIED="1760970669576"/>
+</node>
+</node>
+</node>
+<node TEXT="What does AI do?" ID="ID_644586855" CREATED="1760969352346" MODIFIED="1760969358708">
+<node TEXT="Artificial intelligence machines (researchers call them &quot;AI Services&quot;) don&apos;t think. They calculate." ID="ID_1447152390" CREATED="1760970735978" MODIFIED="1760970760903">
+<node TEXT="They represent some of the newest, most sophisticated calculating machines in human history." ID="ID_955412334" CREATED="1760970773998" MODIFIED="1760970796750"/>
+<node ID="ID_1916648763" CREATED="1760970796909" MODIFIED="1760970813709"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Some can perform what's called <span style="font-weight: bold;">machine learning</span><span style="font-weight: normal;">&#xa0;&#xa0;as they acquire new data.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_668044576" CREATED="1760970813858" MODIFIED="1760970849933"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Others, using calculations arranged in ways inspired by neurons in the human brain, can even perform <span style="font-weight: bold;">deep learning</span><span style="font-weight: normal;">&#xa0;&#xa0;with multiple levels of calculations.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="What Predictions can AI Make?" ID="ID_630374982" CREATED="1760969361366" MODIFIED="1760969368709"/>
+<node TEXT="How is AI Evolving?" ID="ID_1684598201" CREATED="1760969368882" MODIFIED="1760969376319"/>
+</node>
+<node TEXT="1.2 Module 2: What are the three eras of computing?" ID="ID_1458944705" CREATED="1760969184831" MODIFIED="1760969202277">
+<node TEXT="About this Module" ID="ID_1842238592" CREATED="1760969377485" MODIFIED="1760969384591"/>
+<node TEXT="The Era of Tabulation" ID="ID_1424750759" CREATED="1760969386011" MODIFIED="1760969391961"/>
+<node TEXT="The Era of Programming" ID="ID_1304115411" CREATED="1760969393026" MODIFIED="1760969401923"/>
+<node TEXT="The Era of AI" ID="ID_1072584112" CREATED="1760969468608" MODIFIED="1760969472329"/>
+</node>
+<node TEXT="1.3 Module 3: Structured, Semi-Structured, and Unstructured Data" ID="ID_1911958597" CREATED="1760969208328" MODIFIED="1760969229479">
+<node TEXT="About this Module" ID="ID_86148138" CREATED="1760969377485" MODIFIED="1760969384591"/>
+<node TEXT="A look at the types of Data" ID="ID_93781896" CREATED="1760969485637" MODIFIED="1760969495031"/>
+<node TEXT="Analyzing Unstructured Data" ID="ID_914077759" CREATED="1760969495205" MODIFIED="1760969500891"/>
+</node>
+<node TEXT="1.4 Module 4: Is Machine Learning the Answer to the Unstructured Data Problem?" ID="ID_813462937" CREATED="1760969229651" MODIFIED="1760969253924">
+<node TEXT="About this Module" ID="ID_450361390" CREATED="1760969377485" MODIFIED="1760969384591"/>
+<node TEXT="How does ML Approach a Problem?" ID="ID_1256050105" CREATED="1760969618384" MODIFIED="1760969627462"/>
+<node TEXT="ML uses Probabilistic Calculation" ID="ID_840360402" CREATED="1760969627968" MODIFIED="1760969658110"/>
+</node>
+<node TEXT="1.5 Module 5: Three Common Methods of Machine Learning" ID="ID_809532139" CREATED="1760969254108" MODIFIED="1760969268949">
+<node TEXT="About this Module" ID="ID_1173629333" CREATED="1760969377485" MODIFIED="1760969384591"/>
+<node TEXT="Supervised Learning" POSITION="bottom_or_right" ID="ID_1919395674" CREATED="1760969504716" MODIFIED="1760969571984"/>
+<node TEXT="Unsupervised Learning" POSITION="bottom_or_right" ID="ID_1011951071" CREATED="1760969572403" MODIFIED="1760969579783"/>
+<node TEXT="Reinforcement Learning" POSITION="bottom_or_right" ID="ID_483196996" CREATED="1760969579960" MODIFIED="1760969586923"/>
+</node>
+<node TEXT="1.6 Module 6: How will Machine Learning Transform Human Life?" ID="ID_793793806" CREATED="1760969272092" MODIFIED="1760969290295">
+<node TEXT="About this Module" ID="ID_986919977" CREATED="1760969377485" MODIFIED="1760969384591"/>
+<node TEXT="Take another look at the Three Levels of AI" POSITION="bottom_or_right" ID="ID_1840222608" CREATED="1760969589461" MODIFIED="1760969599036"/>
+</node>
+<node TEXT="1.7 Summary and Final Assessment" ID="ID_869281935" CREATED="1760969272248" MODIFIED="1760969305633"/>
+</node>
 <node TEXT="2. Natural Language Processing and Computer Vision" POSITION="bottom_or_right" ID="ID_139914149" CREATED="1760922097728" MODIFIED="1760922111707"/>
 <node TEXT="3. Machine Learning and Deep Learning" POSITION="bottom_or_right" ID="ID_1671207914" CREATED="1760922111857" MODIFIED="1760922118641"/>
 <node TEXT="4. Run AI Models with IBM Watson Studio" POSITION="bottom_or_right" ID="ID_1894139975" CREATED="1760922118795" MODIFIED="1760922128631"/>
