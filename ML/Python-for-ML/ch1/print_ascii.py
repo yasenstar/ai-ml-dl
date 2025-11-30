12f31fbe18c13b8fe1f59a92774feca899a96301
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-11-30 22:26:47 +0800

    add learning ch1

---
+++ ML/Python-for-ML/ch1/print_ascii.py
@@ -0,0 +1,23 @@
+result = ""
+for x in range(65,91):
+    # print(x, chr(x))
+    result = result + chr(x) + " "
+print("result: ", result)
+
+result = ""
+for x in range(97,123):
+    # print(x, chr(x))
+    result = result + chr(x) + " "
+print("result: ", result)
+
+result = ""
+for x in range(ord('A'), ord('Z')+1):
+    # print(x, chr(x))
+    result = result + chr(x) + " "
+print("result: ", result)
+
+result = ""
+for x in range(ord('a'), ord('z')+1):
+    # print(x, chr(x))
+    result = result + chr(x) + " "
+print("result: ", result)
---
+++ ML/Python-for-ML/ch1/test_module.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    print("Hello Small World")
+else:
+    print("Hello Big World")
